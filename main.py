6cfe084815e30117c069046ecc0ea71815172968
Spotika <andrey.a.cherkashin@gmail.com>
2022-12-06 00:45:30 +1000

    second commit

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
-# from PyQt5 import QtWidgets
+import sys
+import serial.tools.list_ports
+import time
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
 from Ui_ykazka import Ui_Ykazka
 
-import sys
-
-
-
-class App(QMainWindow, Ui_Ykazka):
+class AppPult(QMainWindow, Ui_Ykazka):
+    
+    sensitivity: int = 1
+    """min 1, max 100"""
+    
+    
     def __init__(self):
         super().__init__()
-        
         self.setupUi(self)
         
+        # привязка событий
+        self.sensitivitySlider.valueChanged.connect(self.sensitivity_set)
+        
+        self.applyBtn.clicked.connect(self.apply_btn_clicked)
+        
+        self.connectBtn.clicked.connect(self.connect_btn_clicked)
 
+        self.comPortsList.view().pressed.connect(self.apply_btn_clicked)
+
+        self.comPortsList.mousePressEvent = self.com_ports_update
+        
+        self.add_com_ports()
+
+
+    # события
+    def sensitivity_set(self, value):
+        self.sensitivity = value
+        self.SensitivityValue.setText(f"{self.sensitivity}%")
+        
+
+    def com_ports_update(self, v):
+        print(v)
+
+
+    def connect_btn_clicked(self):
+        pass
+
+
+    def apply_btn_clicked(self, *args):
+        self.outputText.append("OK")
+
+        
+        
+        
+        
+
+        
 
 
 def main():
     app = QApplication(sys.argv)
     
-    mainWindow = App()
-    
+    mainWindow = AppPult()
+
     mainWindow.show()
     
     sys.exit(app.exec_())
@@ -29,18 +68,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-# def main():
-#     app = QApplication(sys.argv)
-    
-#     window = QMainWindow()    
-    
-    
-#     window.setWindowTitle("test title")
-#     window.setGeometry(0, 400, 500, 500)
-    
-#     window.show()
-    
-#     sys.exit(app.exec_())
-    
-# if __name__ == "__main__":
-#     main()
